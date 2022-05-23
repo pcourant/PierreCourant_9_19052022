249c1f70b63dfcda11127c0a33a71177fb9d854a
Pierre COURANT <pcourant.dev@gmail.com>
2022-05-23 08:17:20 +0200

    Fix bug [Employee] only a image JPG or PNG can be upload in the new bill form

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -17,28 +17,37 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const fileElement = this.document.querySelector(`input[data-testid="file"]`)
+    const file = fileElement.files[0]
     const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    const fileName = filePath[filePath.length - 1]
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+    const regexJPGorPNG = /^.*\.(jpg|JPG|jpeg|JPEG|png|PNG)$/
+    if (!fileName.match(regexJPGorPNG)) {
+      fileElement.value=''
+      fileElement.setCustomValidity("Erreur de format. Seuls les fichiers jpg ou png sont acceptés.")
+      fileElement.reportValidity()
+    } else {
+      const formData = new FormData()
+      const email = JSON.parse(localStorage.getItem("user")).email
+      formData.append('file', file)
+      formData.append('email', email)
+
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({ fileUrl, key }) => {
+          console.log(fileUrl)
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
